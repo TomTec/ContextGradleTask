13ff23dd59466cb50b5197225656b4c8eb5dd0bd
Romanow <Romanow88@gmail.com>
2020-02-05 23:46:38 +0100

    Switch to gradle for build Update the actions to use the new APIs for data keys and modules Copy over the RunGradleTaskDialog and RunGradleTaskHistoryService from the Gradle plugin update plugin xml

---
+++ src/main/java/de/tomtec/idea/plugin/gradle/run/service/RunGradleTaskHistoryService.java
@@ -0,0 +1,91 @@
+package de.tomtec.idea.plugin.gradle.run.service;
+
+import com.intellij.openapi.components.*;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.gradle.service.task.ExecuteGradleTaskHistoryService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * original class taken from org.jetbrains.plugins.gradle.service.task.RunGradleTaskHistoryService
+ * @author Vladislav.Soroka
+ */
+@State(
+        name = "runGradleTaskHistory",
+        storages = @Storage(StoragePathMacros.WORKSPACE_FILE)
+)
+public class RunGradleTaskHistoryService implements PersistentStateComponent<String[]> {
+    private static final int MAX_HISTORY_LENGTH = 20;
+    private final LinkedList<String> myHistory = new LinkedList<>();
+    private String myWorkDirectory = "";
+    private String myCanceledCommand;
+
+    public static RunGradleTaskHistoryService getInstance(@NotNull Project project) {
+        return ServiceManager.getService(project, RunGradleTaskHistoryService.class);
+    }
+
+    @Nullable
+    public String getCanceledCommand() {
+        return myCanceledCommand;
+    }
+
+    public void setCanceledCommand(@Nullable String canceledCommand) {
+        myCanceledCommand = canceledCommand;
+    }
+
+    public void addCommand(@NotNull String command, @NotNull String projectPath) {
+        myWorkDirectory = projectPath.trim();
+
+        command = command.trim();
+
+        if (command.length() == 0) return;
+
+        myHistory.remove(command);
+        myHistory.addFirst(command);
+
+        while (myHistory.size() > MAX_HISTORY_LENGTH) {
+            myHistory.removeLast();
+        }
+    }
+
+    public List<String> getHistory() {
+        return new ArrayList<>(myHistory);
+    }
+
+    @NotNull
+    public String getWorkDirectory() {
+        return myWorkDirectory;
+    }
+
+    @Nullable
+    @Override
+    public String[] getState() {
+        String[] res = new String[myHistory.size() + 1];
+        res[0] = myWorkDirectory;
+
+        int i = 1;
+        for (String goal : myHistory) {
+            res[i++] = goal;
+        }
+
+        return res;
+    }
+
+    @Override
+    public void loadState(@NotNull String[] state) {
+        if (state.length == 0) {
+            myWorkDirectory = "";
+            myHistory.clear();
+        }
+        else {
+            myWorkDirectory = state[0];
+            myHistory.addAll(Arrays.asList(state).subList(1, state.length));
+        }
+    }
+}
